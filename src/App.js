c2cf072b4178bb85c9f19d9e5f6f4e10af2dde38
Nacho Comelles <ignaciocomelles@gmail.com>
2022-10-29 21:59:44 -0300

    prueba front conextado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import logo from './cloud.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo}  alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Paginita para cloud
         </p>
         <a
           className="App-link"
