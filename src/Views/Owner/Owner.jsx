f420eb547856f0a704e550078481056479922bc3
Noceloni <julian_noceloni@hotmail.com>
2022-11-07 13:40:15 -0300

    Agrego pagina de inicio del usuario owner

---
+++ src/Views/Owner/Owner.jsx
@@ -0,0 +1,14 @@
+import '../../App.css';
+
+const Owner = () => {
+
+    return(
+            <div class="fondo">
+                <h1 class="texto">Bienvenido/a!</h1>
+                <div><a class="texto" href="/crear-plato">Crear Plato</a></div>
+                <div><a class="texto" href="/mis-platos">Mis platos</a></div>
+            </div>
+        );
+    };
+
+export default Owner;
