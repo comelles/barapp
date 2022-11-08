85d31c43b16f68bcb24c4eba33d482ddc9904e21
Noceloni <julian_noceloni@hotmail.com>
2022-11-08 13:02:03 -0300

    Agrego botones con links a views

--- src/Views/Owner/Owner.jsx
+++ src/Views/Owner/Owner.jsx
@@ -1,13 +1,23 @@
 import '../../App.css';
+import { useState, useEffect, Fragment, useRef } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Owner = () => {
 
     return(
-            <div class="fondo">
-                <h1 class="texto">Bienvenido/a!</h1>
-                <div><a class="texto" href="/crear-plato">Crear Plato</a></div>
-                <div><a class="texto" href="/mis-platos">Mis platos</a></div>
+        <Fragment>
+        <div class="container rounded bg-white mt-5 mb-5">
+          <h1>Bienvenido!</h1> 
+            <div class="p-2 py-6">
+                <div class="d-flex justify-content-center align-items-center mb-3">
+                    <a className="btn btn-danger" type="button" class="btn btn-danger btn-lg btn-block" href="/mis-platos">Ver platos creados</a>
+                </div>
+                <div class="d-flex justify-content-center align-items-center mb-3">
+                    <a className="btn btn-danger" type="button" class="btn btn-danger btn-lg btn-block" href="/crear-plato">Agregar nuevo plato</a>
+                </div>
             </div>
+        </div>
+      </Fragment>
         );
     };
 
