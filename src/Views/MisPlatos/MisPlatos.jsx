95796c7802abe60a0cb3062d3b77091f07169c59
Noceloni <julian_noceloni@hotmail.com>
2022-11-17 15:02:19 -0300

    Elimino tabla

--- src/Views/MisPlatos/MisPlatos.jsx
+++ src/Views/MisPlatos/MisPlatos.jsx
@@ -5,13 +5,8 @@ import Swal from 'sweetalert2'
 import DeleteIcon from '@mui/icons-material/Delete';
 import styled from 'styled-components';
 import Modal from './Modal'
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import * as React from 'react';
 import { styled as styled2 } from '@mui/material/styles';
 import Card from '@mui/material/Card';
@@ -24,39 +19,14 @@ import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red, green } from '@mui/material/colors';
-import ShareIcon from '@mui/icons-material/Share';
 import SendIcon from '@mui/icons-material/Send';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Fab from '@mui/material/Fab';
 import EditIcon from '@mui/icons-material/Edit';
-import { CompressOutlined } from '@mui/icons-material';
-import hamburguesa from '../../imagenes/hamburguesa.png';
 import { API } from 'aws-amplify';
 import * as queries from '../../graphql/queries';
 import * as mutations from '../../graphql/mutations';
 import * as subscriptions from '../../graphql/subscriptions';
-
-
-const StyledTableCell = styled2(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-      backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
-    },
-    [`&.${tableCellClasses.body}`]: {
-      fontSize: 14,
-    },
-  }));
-  
-  const StyledTableRow = styled2(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
-    },
-    // hide last border
-    '&:last-child td, &:last-child th': {
-      border: 0,
-    },
-  }));
-
 
 const ExpandMore = styled2((props) => {
     const { expand, ...other } = props;
@@ -71,7 +41,6 @@ const ExpandMore = styled2((props) => {
 
 export const MisPlatos = () => {
 
-
     const [listComidas, setListComidas] = useState([]);
     useEffect(() =>{
         async function getAllComidas(){
@@ -80,12 +49,10 @@ export const MisPlatos = () => {
         }
         getAllComidas();
     
-    
     }, []);
     //console.log(listComidas[0].Nombre)
 
     //como obtener el listado de comidas 
-
 
     const [expanded, setExpanded] = React.useState(false);
     const [estadoModal, cambiarEstadoModal] = useState(false);
@@ -102,52 +69,13 @@ export const MisPlatos = () => {
     return(
         <Fragment>
             <Header />
-                <div className="container p-3">
-                    <TableContainer component={Paper}>
-                        <Table sx={{ minWidth: 5 }} aria-label="customized table">
-                            <TableHead>
-                                <TableRow>
-                                    <StyledTableCell>Plato</StyledTableCell>
-                                    <StyledTableCell>Precio</StyledTableCell>
-                                    {/*<StyledTableCell>Fecha de fin</StyledTableCell>*/}
-                                    <StyledTableCell>Descripcion</StyledTableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {/* {array.map((item) => ( */}
-                                    <StyledTableRow>
-                                        <StyledTableCell component="th" scope="row">a</StyledTableCell>
-                                        <StyledTableCell>b</StyledTableCell>
-                                        <StyledTableCell>c</StyledTableCell>
-                                        {/*<StyledTableCell>
-                                            <select
-                                                className="custom-select my-1 mr-2"
-                                                id="inlineFormCustomSelect"
-                                                name="team"
-                                                value="d"
-                                                onChange={handleChange}
-                                            >
-                                            <option default>abc</option>
-                                            </select>
-                                        </StyledTableCell>
-                                        <StyledTableCell>
-                                            <button  className="btn btn-success mt-2 mb-2 mx-2 h-25">Enviar solicitud</button>
-                                        </StyledTableCell>*/}
-                                    </StyledTableRow>
-                                        {/*))*/}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                        
-                    </div>
-                    <div class="container">
-                        <div class="d-flex overflow-scroll mt-3">
-                                {listComidas && listComidas.map(item => { 
-                                    
-                                    return(
-                                    <div class="d-flex col-md-3">
-                                        <div>
-                                            <Card sx={{ maxWidth: 345 }}>
+                <div class="container">
+                    <div class="d-flex overflow-scroll mt-3">
+                        {listComidas && listComidas.map(item => {        
+                            return(
+                                <div class="d-flex col-md-3">
+                                    <div>
+                                        <Card sx={{ maxWidth: 345 }}>
                                             <CardHeader
                                                 avatar={
                                                 <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
@@ -173,41 +101,37 @@ export const MisPlatos = () => {
                                                 </Typography>
                                             </CardContent>
                                             <CardActions disableSpacing>
-                                            
                                             <Fab size="small" sx={{ bgcolor: red[500] }} color="primary" aria-label="delete" >
-                                            <DeleteIcon /*onClick={() => confirmacionDelete(item.id)}*/ />
+                                                <DeleteIcon /*onClick={() => confirmacionDelete(item.id)}*/ />
                                             </Fab>
-                                        
                                             &nbsp;&nbsp;
                                             <Fab size="small" sx={{ bgcolor: green[500] }} color="primary" aria-label="edit">
-                                            <EditIcon /*onClick={() => cambiarEstadoModal(!estadoModal)}*/ />
+                                                <EditIcon /*onClick={() => cambiarEstadoModal(!estadoModal)}*/ />
                                             </Fab>
                                                 <ExpandMore
-                                                expand={expanded}
-                                                onClick={handleExpandClick}
-                                                aria-expanded={expanded}
-                                                aria-label="show more"
-                                                >
-                                                <ExpandMoreIcon />
+                                                    expand={expanded}
+                                                    onClick={handleExpandClick}
+                                                    aria-expanded={expanded}
+                                                    aria-label="show more"
+                                                    >
+                                                    <ExpandMoreIcon />
                                                 </ExpandMore>
                                             </CardActions>
                                             <Collapse in={expanded} timeout="auto" unmountOnExit>
                                                 <CardContent>
-                                                <Typography paragraph>Descripcion:</Typography>
-                                                <Typography paragraph> {item.Descripcion}
-                                                </Typography>
+                                                    <Typography paragraph>Descripcion:</Typography>
+                                                    <Typography paragraph> {item.Descripcion}</Typography>
                                                 </CardContent>
                                             </Collapse>
-                                            </Card>
-                                        </div> 
-                                        
+                                        </Card>
                                     </div> 
-                                )})}
-                            </div>
+                                </div> 
+                            )})}
+                        </div>
                     </div>
-    </Fragment>
-           );
-    };
+        </Fragment>
+    );
+};
 
 export default MisPlatos;
 
